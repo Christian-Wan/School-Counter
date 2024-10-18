d8821f482ca65339a8a6221e5176018b27b60b0c
Christian-Wan <145377201+Christian-Wan@users.noreply.github.com>
2024-10-18 10:17:31 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 //This is the student counter app
 var count = 0
-
+var saved = ""
 function increment() {
     count++
     document.getElementById("count-el").innerText = count
+}
 
+function addSave() {
+    if (saved != "") {
+        saved += "-"
+    }
+    saved += count
+    document.getElementsByTagName("p")[0].innerHTML = "Previous Enteries: " + saved
 }
